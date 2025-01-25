027177324efab79505f1850b6bcfa412b1e01475
Emerson Wanderlei <emerson.wanderlei03@gmail.com>
2025-01-25 19:25:26 -0300

    update

--- crud_wandeco/routes.py
+++ crud_wandeco/routes.py
@@ -11,17 +11,6 @@ def home():
 
     return render_template('home.html', usuario=usuario, formCreateUser=formCreateUser)
 
-@app.route('/update/<int:user_id>', methods=['POST'])
-def update(user_id):
-    user = Usuario.query.get(user_id)
-    if user and 'button_update':
-        database.session.add(user)
-        database.session.commit()
-        return redirect(url_for('home'))
-
-
-    return redirect(url_for('home'))
-
 @app.route('/create', methods=['POST'])
 def create():
     formCreateUser = Form_create_user()
@@ -36,5 +25,35 @@ def delete(user_id):
     user = Usuario.query.get(user_id)
     if user and 'button_delete':
         database.session.delete(user)
+        database.session.commit()
+    return redirect(url_for('home'))
+
+@app.route('/edit/<int:user_id>', methods=['GET', 'POST'])
+def edit(user_id):
+    user = Usuario.query.get(user_id)
+    if not user:
+        return redirect(url_for('home'))
+    formCreateUser = Form_create_user()
+    if user and 'button_update':
+        formCreateUser.name.data = user.name
+        formCreateUser.email.data = user.email
+        formCreateUser.phone.data = user.phone
+
+    usuarios = Usuario.query.all()
+    return render_template('home.html', usuario=usuarios, formCreateUser=formCreateUser, editing=True, user_id=user_id)
+
+
+@app.route('/update/<int:user_id>', methods=['POST'])
+def update(user_id):
+    user = Usuario.query.get(user_id)
+    if not user:
+        return redirect(url_for('home'))
+    
+    formCreateUser = Form_create_user()
+    if formCreateUser.validate_on_submit():
+        user.name = formCreateUser.name.data
+        user.email = formCreateUser.email.data
+        user.phone = formCreateUser.phone.data
+
         database.session.commit()
     return redirect(url_for('home'))
